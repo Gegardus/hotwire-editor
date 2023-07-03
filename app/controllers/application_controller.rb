0517a6c7462ab637671c66a4fc2e098a783a0bae
Gegardus <stgegard@gmail.com>
2023-07-03 13:45:35 +0400

    add action cable with turbo_stream

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,25 @@ class ApplicationController < ActionController::Base
   # Add this line to see the progress bar long enough
   # and remove it when it has the expected styles
   # before_action -> { sleep 3 }
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_user!, unless: :devise_controller?
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:company_id, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:company_id, :email, :password, :password_confirmation, :current_password)
+    end
+  end
+
+  private
+  
+  def current_company
+    @current_company ||= current_user.company if user_signed_in?
+  end
+  helper_method :current_company
 end
